c92389457519ff9bc723c62688096edda8c5548e
AnyukiOyster <anyukioyster@gmail.com>
2024-11-22 13:54:59 +0500

    'Homework 22/11'

--- bot.py
+++ bot.py
@@ -6,23 +6,37 @@ bot = telebot.TeleBot('8164538734:AAEtE9H4ex65jGQb8W-U6cOcLiDBIg')
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.from_user.id
-    if datab.check_user(user_id):
-        user_name = datab.get_name(user_id)
-        bot.send_message(user_id, f'{user_name}, с возвращением!', reply_markup=telebot.types.ReplyKeyboardRemove())
-    else:
-        bot.send_message(user_id, f'Приветствуем!\nЧтобы продолжить, введите своё имя.',
-                         reply_markup=telebot.types.ReplyKeyboardRemove())
-        bot.register_next_step_handler(message, get_name)
+    bot.send_message(user_id, f'Чтобы продолжить, выберите язык.\n\nChoose language to continue.', reply_markup=buttons.lang_button())
+    bot.register_next_step_handler(message, choose_lang)
 
-@bot.message_handler(content_types=['text'])
-def get_name(message):
+def choose_lang(message):
+    user_id = message.from_user.id
+    if message.text == 'Русский':
+        if datab.check_user(user_id):
+            user_name = datab.get_name(user_id)
+            bot.send_message(user_id, f'{user_name}, с возвращением!', reply_markup=telebot.types.ReplyKeyboardRemove())
+        else:
+            bot.send_message(user_id, f'Приветствуем!\nЧтобы продолжить, введите своё имя.', reply_markup=telebot.types.ReplyKeyboardRemove())
+            bot.register_next_step_handler(message, get_name_ru)
+    elif message.text == 'English':
+        if datab.check_user(user_id):
+            user_name = datab.get_name(user_id)
+            bot.send_message(user_id, f'Welcome back, {user_name}!', reply_markup=telebot.types.ReplyKeyboardRemove())
+        else:
+            bot.send_message(user_id, f'Hello!\nPlease enter your name.', reply_markup=telebot.types.ReplyKeyboardRemove())
+            bot.register_next_step_handler(message, get_name_en)
+    else:
+        bot.send_message(user_id, f'Введена некорректная команда. Чтобы продолжить, выберите язык с помощью одной из кнопок ниже.\n\n'
+                                      f'Incorrect input. Please choose your preferred language via buttons below.')
+
+def get_name_ru(message):
     user_id = message.from_user.id
     user_name = message.text
     bot.send_message(user_id, f'{user_name}, приятно познакомиться! 😊\n'
-                              f'Отправьте нам свой номер телефона, чтобы мы могли связаться с вами.', reply_markup=buttons.tel_button())
-    bot.register_next_step_handler(message, get_tel, user_name)
+                              f'Отправьте нам свой номер телефона, чтобы мы могли связаться с вами.', reply_markup=buttons.tel_button_ru())
+    bot.register_next_step_handler(message, get_tel_ru, user_name)
 
-def get_tel(message, user_name):
+def get_tel_ru(message, user_name):
     user_id = message.from_user.id
     if message.contact:
         user_tel = message.contact.phone_number
@@ -30,6 +44,23 @@ def get_tel(message, user_name):
         bot.send_message(user_id, 'Ура! Регистрация пройдена! 🥳', reply_markup=telebot.types.ReplyKeyboardRemove())
     else:
         bot.send_message(user_id, 'Отправьте контакт через кнопку "Отправить номер" или прикрепите его в виде вложения через скрепку.')
-        bot.register_next_step_handler(message, get_tel, user_name)
+        bot.register_next_step_handler(message, get_tel_ru, user_name)
+
+def get_name_en(message):
+    user_id = message.from_user.id
+    user_name = message.text
+    bot.send_message(user_id, f'Nice to meet you, {user_name}! 😊\n'
+                              f'Please send us your number so we could get in tough with you.', reply_markup=buttons.tel_button_en())
+    bot.register_next_step_handler(message, get_tel_en, user_name)
+
+def get_tel_en(message, user_name):
+    user_id = message.from_user.id
+    if message.contact:
+        user_tel = message.contact.phone_number
+        datab.register(user_id, user_name, user_tel)
+        bot.send_message(user_id, 'Registration is complete! 🥳', reply_markup=telebot.types.ReplyKeyboardRemove())
+    else:
+        bot.send_message(user_id, 'To send you contact information use the "Send my number" button or send it as an attachment.')
+        bot.register_next_step_handler(message, get_tel_en, user_name)
 
 bot.polling(non_stop=True)
--- buttons.py
+++ buttons.py
@@ -1,7 +1,27 @@
 from telebot import types
 
-def tel_button():
+def lang_button():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    lang1 = types.KeyboardButton('Русский')
+    lang2 = types.KeyboardButton('English')
+    kb.add(lang1, lang2)
+    return kb
+
+def lang_buttons():
+    kb = types.InlineKeyboardMarkup(row_width=2)
+    lang1 = types.InlineKeyboardButton(text='Русский', callback_data='ru')
+    lang2 = types.InlineKeyboardButton(text='English', callback_data='en')
+    kb.add(lang1, lang2)
+    return kb
+
+def tel_button_ru():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('Отправить номер 📞', request_contact=True)
+    kb.add(item1)
+    return kb
+
+def tel_button_en():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Send my number 📞', request_contact=True)
     kb.add(item1)
     return kb
